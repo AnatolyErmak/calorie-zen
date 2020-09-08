7f2d23347cd73e80bdf5523c9cfc8fd50c51b69c
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-09-08 15:47:52 +0300

    импорт изображений

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import headerLogo from '../images/calorieZen.png';
 import React from 'react';
 
  function Header() {
    return (
      <div>
+       <img src={headerLogo} />
        <h1>Calorie-Zen</h1>
      </div>
    );
